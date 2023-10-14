ec34cbbde2d4c2bbcdd39c20af8ef5c83bb35838
amsowmya <soumya.pdit@gmail.com>
2023-10-14 15:59:33 +0530

    model evaluation added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import dataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -27,6 +29,26 @@ STAGE_NAME = "Data Transformation stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     obj = dataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nz================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nz================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model evaluation stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = ModelEvaluationTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nz================x")
 except Exception as e:
